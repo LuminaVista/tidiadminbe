f989dfcf29581dbee4fe6b85feb30a5239d2c2ac
Shafayet Ul Islam <shafayet.islam.61@gmail.com>
2025-10-08 13:33:55 +1030

    initial setup done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express from 'express';
+import cors from 'cors';
+import testRoutes from './routes/test.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// Test routes
+app.use('/api/test', testRoutes);
+
+// Health check route
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running' });
+});
+
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on port 3000");
+});
